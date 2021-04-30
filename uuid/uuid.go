2601042c3ba202d2f3eef41065cff5777859fc30
Max Ekman <max@looplab.se>
2021-04-30 16:15:58 +0200

    TBS: Use legacy EH UUID package

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -2,27 +2,34 @@
 // Forks of Event Horizon can re-implement this package with a UUID library of choice.
 package uuid
 
-import "github.com/google/uuid"
+import (
+	"github.com/looplab/eventhorizon/uuid/legacy"
+)
 
 // UUID is an alias type for github.com/google/uuid.UUID.
-type UUID = uuid.UUID
+type UUID = legacy.UUID
 
 // Nil is an empty UUID.
-var Nil = UUID(uuid.Nil)
+var Nil = UUID("")
 
 // New creates a new UUID.
 func New() UUID {
-	return UUID(uuid.New())
+	return UUID(legacy.NewUUID())
 }
 
 // Parse parses a UUID from a string, or returns an error.
 func Parse(s string) (UUID, error) {
-	id, err := uuid.Parse(s)
+	id, err := legacy.ParseUUID(s)
 
 	return UUID(id), err
 }
 
 // MustParse parses a UUID from a string, or panics.
 func MustParse(s string) UUID {
-	return UUID(uuid.MustParse(s))
+	id, err := legacy.ParseUUID(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return UUID(id)
 }
